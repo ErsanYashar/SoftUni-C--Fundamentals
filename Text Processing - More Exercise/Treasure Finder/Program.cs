1dcbc4346b3ae6e6d8b9aa2f9721019ba9739216
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2021-02-01 00:10:13 +0200

    More Exercise

---
+++ Text Processing - More Exercise/Treasure Finder/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Treasure_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(int.Parse)
+                                   .ToArray();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "find")
+            {
+                string newInput = string.Empty;
+
+                int countKeys = 0;
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (countKeys >= numbers.Length)
+                    {
+                        countKeys = 0;
+                    }
+
+                    newInput += (char)(input[j] - numbers[countKeys]);
+                    countKeys++;
+                }
+                string type = string.Empty;
+                int startIndex = newInput.IndexOf('&');
+
+                for (int i = startIndex; i < newInput.Length; i++)
+                {
+                    if(newInput[i + 1] == '&')
+                    {
+                        break;
+                    }
+
+                    type += newInput[i + 1];
+                }
+
+                int indexOfCords = newInput.IndexOf('<');
+                int endIndexOfCords = newInput.IndexOf('>');
+
+                string coordinates = newInput.Substring(indexOfCords + 1, (endIndexOfCords - indexOfCords) - 1);
+
+                Console.WriteLine($"Found {type} at {coordinates}");
+            }
+        }
+    }
+}
